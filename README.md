8601bbeaf02b780d7f10040cabc4ef57ac5f029f
aryojaya12 <aryodewantoro17@gmail.com>
2021-01-27 09:18:33 +0700

    Sticker Wa

---
+++ README.md
@@ -0,0 +1,170 @@
+<p align="center">
+<img src="https://img0.imguh.com/2021/01/16/IMG-20210116-WA0004c38b398fcfdcc2e9.jpg" width="128" height="128"/>
+</p>
+<p align="center">
+<a href="#"><img title="WhatsappBot" src="https://img.shields.io/badge/Whatsapp Bot-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="https://github.com/aryojaya12"><img title="Author" src="https://img.shields.io/badge/Author-aryo dewantoro-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+## Getting Started
+
+This project require NodeJS v12.
+
+### Install
+Clone this project
+
+```bash
+> git clone https://github.com/aryojaya12/StickerWa.git
+> cd StickerWa
+```
+
+Install the dependencies:
+
+```bash
+> npm install 
+> npm install gify-cli -g
+```
+
+### Usage
+Run the Whatsapp bot
+
+```bash
+> npm start
+```
+
+after running it you need to scan the qr
+
+### Information
+- Change ownerNumber on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/settings/setting.json#L2)
+- Change groupLimit on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/settings/setting.json#L3)
+- Change memberLimit on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/settings/setting.json#L4)
+- Change prefix on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/settings/setting.json#L5)
+- Change menu on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/lib/menu.js#L32)
+- Add kata kasar on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/lib/kataKotor.js#L8)
+- Change all apiKey on [this section](https://github.com/aryojaya12/WhatsappBot/blob/master/settings/api.json)
+- Get Api NoBackground on [this website](https://www.remove.bg/)
+- Get Api ScreenShot on [this website](https://apiflash.com/)
+- Get Api Simi-simi on [this website](https://workshop.simsimi.com/en/)
+
+---
+
+## Features
+
+
+| Creator |Yes|
+| ------------- | ------------- |
+| Respond img to sticker|✅|
+| Respond img to sticker no bg|✅|
+| Respond url to sticker|✅|
+| Respond gif to sticker|✅|
+| Respond giphy url to sticker|✅|
+| Make a meme from photo|✅|
+| Quotes maker result pict|✅|
+| Nulis Bot|✅|
+| Arti nama |✅|
+
+
+| Islam |Yes|
+| ------------- | ------------- |
+| List Surah|✅|
+| Info Surah|✅|
+| Surah|✅|
+| Tafsir Alquran|✅|
+| Alquran Audio/Voice|✅|
+| Jadwal solat|✅|
+
+
+| Downloader |Yes|
+| ------------- | ------------- |
+| Youtube Music |✅|
+| Youtube Video |✅|
+| Facebook |✅|
+| Instagram |✅|
+
+
+| Fun Group! |Yes|
+| ------------- | ------------- |
+| Simi-simi BOT|✅|
+| Anti kata kasar|✅|
+
+
+| Searchs |Yes|
+| ------------- | ------------- |
+| Images |✅|
+| Stalk IG |✅|
+| Wikipedia |✅|
+| Cuaca |✅|
+| Lirik musik |✅|
+| Screenshot|✅|
+| Play music|✅|
+| whats anime|✅|
+
+
+| Random text |Yes|
+| ------------- | ------------- |
+| Pantun|✅|
+| Fakta Menarik|✅|
+| Kata Bijak|✅|
+| Quotes|✅|
+
+
+| Random image |Yes|
+| ------------- | ------------- |
+| Anime |✅|
+| Kpop |✅|
+
+
+| Others |Yes|
+| ------------- | ------------- |
+| Teks to Sound/Voice|✅|
+| Translate teks|✅|
+| Get covid info from map|✅|
+| Covid-19 Indo|✅|
+| Shortlink|✅|
+| Hilih F0nt|✅|
+
+| Groups |Yes|
+| ------------- | ------------- |
+| Owner||
+| Kick all members|✅|
+| Admin||
+| Add user|✅|
+| Kick user|✅|
+| Promote User|✅|
+| Demote User|✅|
+| Mute Group|✅|
+| Change Group icon|✅|
+| Delete bot msg|✅|
+| Tagall/mentions all|✅|
+| Welcome ON/OFF|✅|
+
+
+| Owner bot |Yes|
+| ------------- | ------------- |
+| Broadcast|✅|
+| Leave all group|✅|
+| Delete all msgs|✅|
+| Banned user|✅|
+
+
+## To-Do
+ - Add Media Downloader
+ - Add More Feature
+ - More refactoring
+ 
+---
+
+## Troubleshooting
+Make sure all the necessary dependencies are installed: https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md
+
+Fix Stuck on linux, install google chrome stable: 
+```bash
+> wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+> sudo apt install ./google-chrome-stable_current_amd64.deb
+```
+
+## Thanks to
+- [WA-Automate](https://github.com/open-wa/wa-automate-nodejs)
+- [YogaSakti](https://github.com/YogaSakti/imageToSticker))
+- [ArugaZ](https://github.com/ArugaZ/whatsapp-bot)
---
+++ index.js
@@ -0,0 +1,94 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const figlet = require('figlet')
+const options = require('./utils/options')
+const { color, messageLog } = require('./utils')
+const HandleMsg = require('./HandleMsg')
+
+const start = (aruga = new Client()) => {
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('GUNSY BOT', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color('[DEV]'), color('ARYO DEWANTORO', 'yellow'))
+    console.log(color('[~>>]'), color('BOT Started!', 'green'))
+
+    // Mempertahankan sesi agar tetap nyala
+    aruga.onStateChanged((state) => {
+        console.log(color('[~>>]', 'red'), state)
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') aruga.forceRefocus()
+    })
+
+    // ketika bot diinvite ke dalam group
+    aruga.onAddedToGroup(async (chat) => {
+	const groups = await aruga.getAllGroups()
+	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
+	if (groups.length > groupLimit) {
+	await aruga.sendText(chat.id, `Maaf, grup pada Bot ini penuh \nGrup Maks: ${groupLimit}`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
+	  }) 
+	} else {
+	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
+	    if (chat.groupMetadata.participants.length < memberLimit) {
+	    await aruga.sendText(chat.id, `Maaf, Bot keluar jika anggota grup tidak melebihi ${memberLimit} anggota`).then(() => {
+	      aruga.leaveGroup(chat.id)
+	      aruga.deleteChat(chat.id)
+	    })
+	    } else {
+        await aruga.simulateTyping(chat.id, true).then(async () => {
+          await aruga.sendText(chat.id, `Hai minna~, Im Bot Aryo Dewantoro untuk melihat menu bot kirim *${prefix}menu*`)
+        })
+	    }
+	}
+    })
+
+    // ketika seseorang masuk/keluar dari group
+    aruga.onGlobalParicipantsChanged(async (event) => {
+        const host = await aruga.getHostNumber() + '@c.us'
+		const welcome = JSON.parse(fs.readFileSync('./settings/welcome.json'))
+		const isWelcome = welcome.includes(event.chat)
+		let profile = await aruga.getProfilePicFromServer(event.who)
+		if (profile == '' || profile == undefined) profile = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQcODjk7AcA4wb_9OLzoeAdpGwmkJqOYxEBA&usqp=CAU'
+        // kondisi ketika seseorang diinvite/join group lewat link
+        if (event.action === 'add' && event.who !== host && isWelcome) {
+			await aruga.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await aruga.sendTextWithMentions(event.chat, `Halo, Selamat datang di grup @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
+        }
+        // kondisi ketika seseorang dikick/keluar dari group
+        if (event.action === 'remove' && event.who !== host) {
+			await aruga.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await aruga.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
+        }
+    })
+
+    aruga.onIncomingCall(async (callData) => {
+        // ketika seseorang menelpon nomor bot akan mengirim pesan
+        await aruga.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan dan sistem telah memblokir nomor kamu.\n\n- *Bot*')
+        .then(async () => {
+            // bot akan memblock nomor itu
+            await aruga.contactBlock(callData.peerJid)
+        })
+    })
+
+    // ketika seseorang mengirim pesan
+    aruga.onMessage(async (message) => {
+        aruga.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+            .then((msg) => {
+                if (msg >= 3000) {
+                    console.log('[aruga]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    aruga.cutMsgCache()
+                }
+            })
+        HandleMsg(aruga, message)    
+    
+    })
+	
+    // Message log for analytic
+    aruga.onAnyMessage((anal) => { 
+        messageLog(anal.fromMe, anal.type)
+    })
+}
+
+//create session
+create(options(true, start))
+    .then((aruga) => start(aruga))
+    .catch((err) => new Error(err))
